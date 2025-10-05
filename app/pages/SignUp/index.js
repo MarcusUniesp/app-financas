6eb60ded08ec12be102e4e991806cb3acc67db78
Marcus Vinicius Silva Francisquini <francisquinimarcus@gmail.com>
2025-10-05 20:31:41 -0300

    :sparkles: FEAT: Funcionalidade de criar uma conta

--- app/pages/SignUp/index.js
+++ app/pages/SignUp/index.js
@@ -6,25 +6,54 @@ import {
   SubmitButton,
   SubmitText,
 } from "./styles";
-import { Platform } from 'react-native';
+import { Platform } from "react-native";
+import { AuthContext } from "../../context/auth";
+import { useContext, useState } from "react";
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    if (name === "" || email === "" || password === "") {
+      console.log("Preencha todos os campos");
+      return;
+    }
+    signUp(name, password, email);
+  }
+
   return (
     <BackGround>
       <Container behavior={Platform.OS === "ios" ? "padding" : ""} enabled>
         <AreaInput>
-          <Input placeholder="Seu nome" />
+          <Input
+            placeholder="Seu nome"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Email" />
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Senha" secureTextEntry={true} />
+          <Input
+            placeholder="Senha"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
         </AreaInput>
 
-        <SubmitButton>
+        <SubmitButton onPress={handleSignUp}>
           <SubmitText>Cadastrar</SubmitText>
         </SubmitButton>
       </Container>
